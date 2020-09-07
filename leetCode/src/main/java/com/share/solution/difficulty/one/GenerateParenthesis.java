7642d50d03c079bcd56eefe00dd9292f3047949c
lvguoqing <1029839312@qq.com>
2020-09-07 10:10:06 +0800

    keep leaning

--- leetCode/src/main/java/com/share/solution/difficulty/one/GenerateParenthesis.java
+++ leetCode/src/main/java/com/share/solution/difficulty/one/GenerateParenthesis.java
@@ -14,14 +14,15 @@ public class GenerateParenthesis {
 
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        gen("", result, n, n);
+        //gen("", result, n, n);
 
+        _gen(0, 0, n, "", result);
         return result;
     }
 
     private void gen(String subString, List<String> result, int left, int right) {
         if (left == 0 && right == 0) {
-            System.out.println(subString);
+
             result.add(subString);
             return;
         }
@@ -33,8 +34,53 @@ public class GenerateParenthesis {
         }
     }
 
+    private void _gen(int left, int right, int n, String subString, List<String> result) {
+        if (left == n && right == n) {
+            result.add(subString);
+            System.out.println(subString);
+            return;
+        }
+
+        if (left < n) {
+
+            _gen(left + 1, right, n, subString + "(", result);
+        }
+        if (left > right && right < n) {
+            _gen(left, right + 1, n, subString + ")", result);
+        }
+    }
+
+    public int[] exchange(int[] nums) {
+        int len = nums.length;
+        if (nums == null || len <= 2) {
+            return nums;
+        }
+        // 冒泡的变种
+        for (int i = 0; i < len - 1; i++) {
+            if (nums[i] % 2 == 0) {
+                continue;
+            } else {
+                int ti = i;
+                while (ti < len - 1) {
+
+                    int temp = nums[ti];
+                    nums[ti] = nums[ti + 1];
+                    nums[ti + 1] = temp;
+                    ti++;
+                }
+            }
+        }
+        return nums;
+    }
+
     public static void main(String[] args) {
         GenerateParenthesis generateParenthesis = new GenerateParenthesis();
-        generateParenthesis.generateParenthesis(3);
+
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
+        for (int i : generateParenthesis.exchange(nums)) {
+
+            System.out.println(i);
+        }
+        System.out.println();
     }
 }
